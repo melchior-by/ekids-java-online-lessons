4e82d27e0820ef5629a48db80116055e54ddcb85
Artsiom Yurkevich <Artsiom_Yurkevich@epam.com>
2020-04-05 04:54:12 +0300

    Lesson2: Add info about Stack and Heap in comments. Moved creation of Recipe from Printer class to Main. Offered constructor to Recipe.

--- src/by/epam/ekids/Main.java
+++ src/by/epam/ekids/Main.java
@@ -1,5 +1,6 @@
 package by.epam.ekids;
 
+import by.epam.ekids.recepies.Recipe;
 import by.epam.ekids.recepies.RecipePrinter;
 
 public class Main {
@@ -19,15 +20,48 @@ public class Main {
 
     // !!! ЕСЛИ МЫ МОЖЕМ ВЫДЕЛИТЬ ФУНКЦИОНАЛ - ИСПОЛЬЗУЕМ ИЛИ ОТДЕЛЬНЫЙ МЕТОД ИЛИ СОЗДАЕМ КЛАСС.
 
+    /*
+    *  STACK
+    *  ******
+    *  *  1  *
+    *  *  2  *
+    *  *  3  *
+    *  *  4  * --
+    *  *  5  *  |
+    *  ******   |
+    *           |
+    *   HEAP    |
+     *  ***************
+     *  *  1   52.1    * Double a = 52.1;
+     *  *              *
+     *  *   hi   ""    *
+     *  *     100500   *
+     *  ***************
+    * */
+
     private static String user = "Степан";
     private static int count = 10;
 
     public static void main(String[] args) {
+
         printMe("Hello world!", 4);
 
         printMe(String.format("Привет %s, мы тебя поприветствуем %d раз", user, count), count);
 
-        RecipePrinter.print();
+        // создаем объект рецепта пельменей
+        Recipe pelmeni = new Recipe("ПЕЛЬМЕНИ", new String[]{"мясо", "тесто", "сметанкой"},
+                "Сходить в магазин, купить %s, %s. Слепить пельмени. \nСварить. Полить %s.");
+        Recipe pizza = new Recipe("ПИЦЦУНЯ", new String[]{"тесто", "сыр", "грибочки"},
+                "Взять ингридиенты, замесить %s, добавить %s и %s. Отправить в печь при 120 °C");
+
+        RecipePrinter.print(pelmeni);
+        RecipePrinter.print(pizza);
+
+        // с помощью set методов задаем его поля (ингридиенты и инструкцию по приготовлению)
+        pelmeni.setIngredients(new String[]{"мясо", "тесто", "маянезик"});
+        pelmeni.setInstructions("Надеть маску от короны. Сходить в магазин, купить %s, %s. Слепить пельмени. \nСварить. Полить %s.");
+
+        RecipePrinter.print(pelmeni);
     }
 
     private static void printMe(String message, int howMany) {
--- src/by/epam/ekids/recepies/RecipePrinter.java
+++ src/by/epam/ekids/recepies/RecipePrinter.java
@@ -4,16 +4,14 @@ import java.util.Arrays;
 
 public class RecipePrinter {
 
-    public static void print() {
-        // создаем объект рецепта пельменей
-        Recipe pelmeni = new Recipe();
-        // с помощью set методов задаем его поля (ингридиенты и инструкцию по приготовлению)
-        pelmeni.setIngredients(new String[]{"мясо", "тесто", "сметанкой"});
-        pelmeni.setInstructions("Сходить в магазин, купить %s, %s. Слепить пельмени. \nСварить. Полить %s.");
+    public static void print(Recipe recipe) {
         // сервисные переменные чтобы красиво распечатать
-        String beautyIngredients = "Ингридиенты: " + Arrays.toString(pelmeni.getIngredients()) + ".";
-        String beautyInstruction = String.format(pelmeni.getInstructions(), pelmeni.getIngredients()[0], pelmeni.getIngredients()[1], pelmeni.getIngredients()[2]);
+        String beautyIngredients = "Ингридиенты: " +
+                Arrays.toString(recipe.getIngredients()).replace("[", "").replace("]", "") + ".";
+        String beautyInstruction = String.format(recipe.getInstructions(),
+                recipe.getIngredients()[0], recipe.getIngredients()[1], recipe.getIngredients()[2]);
+
         // основная цель данного метода - напечатать весь рецепт красиво
-        System.out.println("Рецепт ПЕЛЬМЕНЕЙ!!!\n" + beautyIngredients + "\n" + beautyInstruction);
+        System.out.println("\nРецепт " + recipe.getName() + "!!!\n" + beautyIngredients + "\n" + beautyInstruction);
     }
 }
