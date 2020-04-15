9f23cb9c1a53f5b42c803d465d1b2081369fb875
Artsiom Yurkevich <Artsiom_Yurkevich@epam.com>
2020-04-15 15:59:12 +0300

    Lesson4 - intro to inheritance and Interfaces

---
+++ src/by/epam/ekids/recepies/HaveCookingTime.java
@@ -0,0 +1,9 @@
+package by.epam.ekids.recepies;
+
+public interface HaveCookingTime {
+    //возвращаемое значение имя метода
+    //класс который реализует данный интерефейс должен иметь
+    //метод с таким же именем и возвращаемым значением
+    public String getCookingTime();
+    //нет тела - {}
+}
--- src/by/epam/ekids/recepies/RecipeService.java
+++ src/by/epam/ekids/recepies/RecipeService.java
@@ -6,35 +6,40 @@ import java.util.Arrays;
 
 public class RecipeService {
 
-    public static void printRecipe(Recipe recipe) {
+    public static void printRecipe(RecipeRecord recipe) {
         // сервисные переменные чтобы красиво распечатать
-        String beautyIngredients = "Ингридиенты: " +
-                Arrays.toString(recipe.getIngredients()).replace("[", "").replace("]", "") + ".";
-        String beautyInstruction = String.format(recipe.getInstructions(),
-                recipe.getIngredients()[0], recipe.getIngredients()[1], recipe.getIngredients()[2]);
+        String beautyIngredients =
+                "Ингридиенты: " + Arrays.toString(recipe.getIngredients()).replace("[", "").replace("]", "") + ".";
+        String beautyInstruction = String.format(recipe.getInstructions(), recipe.getIngredients()[0], recipe.getIngredients()[1],
+                        recipe.getIngredients()[2]);
 
         // основная цель данного метода - напечатать весь рецепт красиво
         System.out.println("\nРецепт " + recipe.getName() + "!!!\n" + beautyIngredients + "\n" + beautyInstruction);
     }
 
-    public static void printCookingProcess(Recipe recipe, Cook cook) throws InterruptedException {
+    public static void printCookingProcess(CookingRecipe recipe, Cook cook) throws InterruptedException {
         String cookName = cook.getName();
         String recipeName = recipe.getName();
+        String speciality = cook.getSpeciality();
 
-        System.out.println("\nПовар " + cookName + " приступил к приготовлению " + recipeName);
+        System.out.println(
+                "\nПовар " + cookName + " гуру " + speciality + " кухни приступил к приготовлению " + recipeName);
         System.out.println(cookName + " готовит блюдо в следующей последовтельности: ");
 
         // сервисные переменные чтобы красиво распечатать
-        String beautyInstruction = String.format(recipe.getInstructions(),
-                recipe.getIngredients()[0], recipe.getIngredients()[1], recipe.getIngredients()[2]);
+        String beautyInstruction = String
+                .format(recipe.getInstructions(), recipe.getIngredients()[0], recipe.getIngredients()[1], recipe.getIngredients()[2]);
 
         System.out.println(beautyInstruction);
         System.out.println("Время приготовления: " + getTimeToCook(recipe, cook));
-        Thread.sleep(15000);
+        //эмулируем время приготовления
+        Thread.sleep(5000);
         System.out.println("Блюдо " + recipeName + " приготовлено");
+        System.out.println("Зацени какой у нас повар крутой, оригинальное время приготовления рецепта - " + recipe
+                .getCookingTime());
     }
 
-    private static String getTimeToCook(Recipe recipe, Cook cook) {
+    private static String getTimeToCook(CookingRecipe recipe, Cook cook) {
         return String.valueOf(recipe.getTimeToCook().minusMinutes((long) (cook.getCoolness() * 0.2)));
     }
 
